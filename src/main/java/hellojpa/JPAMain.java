a8b5771993a258f995a058a86a8c129ae442b83d
chaerlo127 <monynony0203@gmail.com>
2023-02-02 17:55:21 +0900

    #2 jpql

--- src/main/java/hellojpa/JPAMain.java
+++ src/main/java/hellojpa/JPAMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JPAMain {
     public static void main(String[] args){
@@ -13,10 +14,14 @@ public class JPAMain {
         EntityTransaction tx = em.getTransaction(); // 트랜잭션
         tx.begin();
         try{
-            Member findMember = em.find(Member.class, 1L);
-            // 저장하지 않고, 그대로 둬도 가능
-            // java collection 다루는 것곽 같음
-            findMember.setName("HelloJPA");
+//            Member findMember = em.find(Member.class, 1L);
+            // 대상이 table이 아닌 객체임
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for(Member member: result){
+                System.out.println("member.name = " + member.getName());
+            }
             tx.commit();
         }catch (Exception e){
             tx.rollback();
